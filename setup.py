5743683797ee0a4999350510e7de1e4aeeb073ba
tncga <qokberk_akdeniz@hotmail.com>
2019-07-13 18:11:39 +0300

    use markdown for long description

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setuptools.setup(
     name='soapify',  
     version='1.0.0',
@@ -7,6 +11,8 @@ setuptools.setup(
     author="tncga",
     author_email="qokberk_akdeniz@hotmail.com",
     description="search on your all spotify playlists",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     url="https://github.com/tncga/soap",
     packages=["soapify"],
     install_requires=["click", "spotipy"],
