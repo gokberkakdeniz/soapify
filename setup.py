91bd0a6e5815048ec632efd685f6d3ba7ba2864d
tncga <qokberk_akdeniz@hotmail.com>
2019-07-13 18:10:17 +0300

    remove long description

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import setuptools
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 setuptools.setup(
     name='soapify',  
     version='1.0.0',
@@ -11,7 +7,6 @@ setuptools.setup(
     author="tncga",
     author_email="qokberk_akdeniz@hotmail.com",
     description="search on your all spotify playlists",
-    long_description=readme(),
     url="https://github.com/tncga/soap",
     packages=["soapify"],
     install_requires=["click", "spotipy"],
